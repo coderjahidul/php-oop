56c4869eb992b1208b37531b7036340d678ed1e3
coderjahidul <sobuz0349@gmail.com>
2024-04-04 13:43:32 +0600

    explain classes and objects

---
+++ 01.basich/classes-and-objects.php
@@ -0,0 +1,65 @@
+<?php
+// // Explain Classes and Objects in PHP OOP
+// class Fruit {
+//     // Properties
+//     public $name;
+//     Public $color;
+
+//     // Methods
+//     function set_name($name){
+//         $this->name = $name;
+//     }
+//     function get_name(){
+//         return $this->name;
+//     }
+// }
+
+// $apple = new Fruit();
+// $apple->set_name('Apple');
+// echo $apple->get_name();
+// echo "\n";
+
+// $banana = new Fruit();
+// $banana->set_name('Banana');
+// echo $banana->get_name();
+// echo "\n";
+
+class care {
+    // Properties
+    public $name;
+    public $color;
+    public $modal;
+
+    // Mathods
+    function set_name($name){
+        $this->name = $name;
+    }
+    function set_color($color){
+        $this->color = $color;
+    }
+    function set_modal($modal){
+        $this->modal = $modal;
+    }
+
+    function get_name(){
+        return $this->name;
+    }
+    function get_color(){
+        return $this->color;
+    }
+    function get_modal(){
+        return $this->modal;
+    }
+}
+
+$toyota = new care();
+$toyota->set_name("toyota");
+$toyota->set_color("color");
+$toyota->set_modal("t-584");
+
+echo $toyota->get_name();
+echo "\n";
+echo $toyota->get_color();
+echo "\n";
+echo $toyota->get_modal();
+?>
