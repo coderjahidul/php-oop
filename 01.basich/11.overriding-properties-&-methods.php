a651e9248dd4e9e888cc9ea00792ccc5dc4e382e
coderjahidul <sobuz0349@gmail.com>
2024-05-21 00:34:27 +0600

    add overriding properties and methods file and explain this function

---
+++ 01.basich/11.overriding-properties-&-methods.php
@@ -0,0 +1,30 @@
+<?php
+// Overriding properties
+// class basic {
+//     public $name = "Parent Class";
+// }
+// class derived extends basic {
+//     public $name = "Child Class";
+// }
+
+// $test = new derived();
+// echo $test->name;
+
+// Overriding Methods
+class base {
+    public function calc($a, $b){
+        return $a * $b;
+    }
+}
+
+class derived extends base {
+    public function calc($a, $b){
+        return $a + $b;
+    }
+}
+
+$test = new base();
+echo $test->calc(30, 20);
+echo "\n";
+$one = new derived();
+echo $one->calc(20, 50);
