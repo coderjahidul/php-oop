12bb9775efe52a32cd6d1f1a1c1584a144a26317
coderjahidul <sobuz0349@gmail.com>
2024-05-23 16:27:41 +0600

    add static properties file and explain this funciton

--- 01.basich/11.overriding-properties-&-methods.php
+++ 01.basich/11.overriding-properties-&-methods.php
@@ -11,20 +11,35 @@
 // echo $test->name;
 
 // Overriding Methods
+// class base {
+//     public function calc($a, $b){
+//         return $a * $b;
+//     }
+// }
+
+// class derived extends base {
+//     public function calc($a, $b){
+//         return $a + $b;
+//     }
+// }
+
+// $test = new base();
+// echo $test->calc(30, 20);
+// echo "\n";
+// $one = new derived();
+// echo $one->calc(20, 50);
+
 class base {
     public function calc($a, $b){
         return $a * $b;
     }
 }
 
-class derived extends base {
+class derive extends base {
     public function calc($a, $b){
         return $a + $b;
     }
 }
 
-$test = new base();
-echo $test->calc(30, 20);
-echo "\n";
-$one = new derived();
-echo $one->calc(20, 50);
+$test = new derive();
+echo $test->calc(30, 10);
---
+++ 01.basich/12.static-properties.php
@@ -0,0 +1,37 @@
+<?php
+// Static Properties
+// class base {
+//     public static $name = "Parent Class";
+// }
+// echo base::$name;
+
+// class ip {
+//     public static $value = "127.0.0.1";
+// }
+
+// // get static property
+// echo ip::$value;
+
+// class ip {
+//     public static $value = 3.14549;
+//     public function staticValue(){
+//         return self::$value;
+//     }
+// }
+// $pi = new ip();
+// echo $pi->staticValue();
+
+class pi {
+    public static $value = 3.14549;
+}
+
+class x extends pi {
+    public function staticValue(){
+        return parent::$value;
+    }
+}
+
+echo x::$value;
+echo "\n";
+$x = new x();
+echo $x->staticValue();
